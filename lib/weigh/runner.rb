6f3096225a6582e60b098fd81748c8778f3511c9
Zach Leslie <zach@puppetlabs.com>
2013-06-17 16:41:49 -0700

    use one data blob and return it from the runer.run method

--- lib/weigh/runner.rb
+++ lib/weigh/runner.rb
@@ -13,35 +13,34 @@ module Weigh
 
     def run
       data = {}
-      total_size = 0
-      summary = {}
-      count = 0
+      data[:total_size] = 0
+      data[:summary] = {}
+      data[:count] = 0
+
       sumdep = @flags.depth
 
       @flags.pathlist.each do |p|
         curdep = 0
         Find.find(p) do |path|
-          count += 1
+          data[:count] += 1
           next if FileTest.symlink?(path)
           if FileTest.directory?(path)
             next if p == path
             ret = Weigh::Util.sum_dir(path,@flags.verbose)
             dir_size = ret[:dir_size]
             next if dir_size == 0
-            count += ret[:count]
-            total_size += dir_size
-            summary["#{path}/"] = dir_size
+            data[:count] += ret[:count]
+            data[:total_size] += dir_size
+            data[:summary]["#{path}/"] = dir_size
             Find.prune
           else
             size = FileTest.size(path)
             next if size == 0
-            total_size += size
-            summary["#{path}"] = size
+            data[:total_size] += size
+            data[:summary]["#{path}"] = size
           end
         end
       end
-      data[:summary]    = summary
-      data[:total_size] = total_size
 
       data
     end
