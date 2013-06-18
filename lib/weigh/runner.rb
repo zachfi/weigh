e48b6ed474ccc29d13b5a02233b20cb2f24a6aa3
Zach Leslie <zach@puppetlabs.com>
2013-06-17 17:00:35 -0700

    some cleanup on the runner class

--- lib/weigh/runner.rb
+++ lib/weigh/runner.rb
@@ -12,32 +12,47 @@ module Weigh
     end
 
     def run
-      data = {}
+
+      # Initialize the data blob
+      data              = {}
       data[:total_size] = 0
-      data[:summary] = {}
-      data[:count] = 0
+      data[:summary]    = {}
+      data[:count]      = 0
 
-      sumdep = @flags.depth
-
+      # Dig into each path given
       @flags.pathlist.each do |p|
-        curdep = 0
         Find.find(p) do |path|
           data[:count] += 1
+
+          # Skip symlinks
           next if FileTest.symlink?(path)
+
           if FileTest.directory?(path)
             next if p == path
             ret = Weigh::Util.sum_dir(path,@flags.verbose)
             dir_size = ret[:dir_size]
+
+            # Skip empty directories
             next if dir_size == 0
-            data[:count] += ret[:count]
+
+            data[:count]      += ret[:count]
             data[:total_size] += dir_size
-            data[:summary]["#{path}/"] = dir_size
+
+            # Add a trailing slash to the key for directories
+            pathname = path + "/"
+
+            data[:summary][pathname] = dir_size
+
             Find.prune
           else
+            # Store the size of the current file
             size = FileTest.size(path)
+
+            # Don't count zero sized files
             next if size == 0
+
             data[:total_size] += size
-            data[:summary]["#{path}"] = size
+            data[:summary][path] = size
           end
         end
       end
